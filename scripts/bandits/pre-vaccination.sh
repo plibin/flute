10f020932a5165d405ae7e71101bb3150dd31239
Pieter Libin <pieter.libin@vub.ac.be>
2018-04-12 20:18:27 +0200

    print the params with their name

--- scripts/bandits/pre-vaccination.sh
+++ scripts/bandits/pre-vaccination.sh
@@ -12,16 +12,16 @@ priorities_no_commas=`echo ${priorities} | sed 's/,//g'`
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#echo "workdir:${workdir}"
-#echo "flute_rl_dir:${flute_rl_dir}"
-#echo "flute_rl_bin:${flute_rl_bin}"
-#echo "config_template:${config_template}"
-#echo "seed:${seed}"
-#echo "r0:${r0}"
-#echo "run_length:${run_length}"
-#echo "geo:${geo}"
-#echo "doses:${doses}"
-#echo "priorities:${priorities}"
+echo "work_dir:${work_dir}"
+echo "flute_rl_dir:${flute_rl_dir}"
+echo "flute_rl_bin:${flute_rl_bin}"
+echo "config_template:${config_template}"
+echo "seed:${seed}"
+echo "r0:${r0}"
+echo "run_length:${run_length}"
+echo "geo:${geo}"
+echo "doses:${doses}"
+echo "priorities:${priorities}"
 
 cd ${work_dir}
 
